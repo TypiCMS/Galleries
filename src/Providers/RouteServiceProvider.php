1a5aefafc9300e04eaf74df0765c09bf1da83126
Samuel De Backer <sdebacker@gmail.com>
2015-04-27 15:07:47 +0200

    routes

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 namespace TypiCMS\Modules\Galleries\Providers;
 
-use Config;
-use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Routing\Router;
+use TypiCMS\Facades\TypiCMS;
 
 class RouteServiceProvider extends ServiceProvider {
 
@@ -42,12 +42,13 @@ class RouteServiceProvider extends ServiceProvider {
             /**
              * Front office routes
              */
-            $routes = $this->app->make('TypiCMS.routes');
-            foreach (Config::get('translatable.locales') as $lang) {
-                if (isset($routes['galleries'][$lang])) {
-                    $uri = $routes['galleries'][$lang];
-                    $router->get($uri, array('as' => $lang.'.galleries', 'uses' => 'PublicController@index'));
-                    $router->get($uri.'/{slug}', array('as' => $lang.'.galleries.slug', 'uses' => 'PublicController@show'));
+            if ($page = TypiCMS::getPageLinkedToModule('galleries')) {
+                foreach (config('translatable.locales') as $lang) {
+                    if ($page->hasTranslation($lang)) {
+                        $uri = $page->translate($lang)->uri;
+                        $router->get($uri, ['as' => $lang.'.galleries', 'uses' => 'PublicController@index']);
+                        $router->get($uri.'/{slug}', ['as' => $lang.'.galleries.slug', 'uses' => 'PublicController@show']);
+                    }
                 }
             }
 
