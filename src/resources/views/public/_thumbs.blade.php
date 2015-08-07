a058893696060910c30f593336377f75a1f0555f
Samuel De Backer <sdebacker@gmail.com>
2015-08-07 11:12:10 +0200

    use name for gallery relation

--- src/resources/views/public/_thumbs.blade.php
+++ src/resources/views/public/_thumbs.blade.php
@@ -3,7 +3,7 @@
     @foreach ($model->files as $file)
         <div class="col-xs-6 col-sm-4 col-md-3">
             @if ($file->type == 'i')
-            <a class="fancybox" href="{{ asset($file->path . '/' . $file->file) }}" data-fancybox-group="{{ $model->slug }}">
+            <a class="fancybox" href="{{ asset($file->path . '/' . $file->file) }}" data-fancybox-group="{{ $model->name }}">
                 {!! $file->present()->thumb(370, 370, array(), 'file') !!}
             </a>
             @else
