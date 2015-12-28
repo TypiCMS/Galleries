9cef9da34a738eda0d497b97fadfc7ff401412d8
Samuel De Backer <sdebacker@gmail.com>
2015-12-28 17:22:30 +0100

    classes

--- src/resources/views/public/_galleries.blade.php
+++ src/resources/views/public/_galleries.blade.php
@@ -1,9 +1,9 @@
-    @foreach ($model->galleries as $gallery)
-    <div class="gallery">
-        @if($gallery->title)
-        <h3 class="gallery-title">{{ $gallery->title }}</h3>
-        @endif
-        {!! $gallery->present()->body !!}
-        @include('galleries::public._thumbs', ['model' => $gallery])
-    </div>
-    @endforeach
+@foreach ($model->galleries as $gallery)
+<div class="gallery">
+    @if($gallery->title)
+    <h3 class="gallery-title">{{ $gallery->title }}</h3>
+    @endif
+    {!! $gallery->present()->body !!}
+    @include('galleries::public._thumbs', ['model' => $gallery])
+</div>
+@endforeach
--- src/resources/views/public/_thumbs.blade.php
+++ src/resources/views/public/_thumbs.blade.php
@@ -1,15 +1,15 @@
 @if ($model->files->count())
-    <ul class="gallery-list">
+    <ul class="gallery-files files-list">
     @foreach ($model->files as $file)
-        <li class="gallery-file">
+        <li class="files-list-item">
             @if ($file->type == 'i')
-            <a class="gallery-image fancybox" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'file') !!}" data-fancybox-group="{{ $model->slug }}">
-                <img class="gallery-image-thumb" src="{!! $file->present()->thumbSrc(370, 370, array(), 'file') !!}" alt="{{ $file->alt_attribute }}">
+            <a class="files-list-image fancybox" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'file') !!}" data-fancybox-group="{{ $model->slug }}">
+                <img class="files-list-image-thumb" src="{!! $file->present()->thumbSrc(370, 370, array(), 'file') !!}" alt="{{ $file->alt_attribute }}">
             </a>
             @else
-            <a class="gallery-document" href="{{ asset($file->path . '/' . $file->file) }}" target="_blank">
-                <span class="gallery-document-icon fa fa-file-o fa-3x"></span>
-                <span class="gallery-document-filename">{{ $file->file }}</span> <small class="gallery-document-filesize">({{ $file->present()->filesize }})</small>
+            <a class="files-list-document" href="{{ asset($file->path . '/' . $file->file) }}" target="_blank">
+                <span class="files-list-document-icon fa fa-file-o fa-3x"></span>
+                <span class="files-list-document-filename">{{ $file->file }}</span> <small class="files-list-document-filesize">({{ $file->present()->filesize }})</small>
             </a>
             @endif
         </li>
