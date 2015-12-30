5da154826897f704f78d670fae838deb2f65933d
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 16:43:06 +0100

    admin controller docblocks

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Galleries\Http\Controllers;
 
 use TypiCMS\Modules\Core\Http\Controllers\BaseAdminController;
 use TypiCMS\Modules\Galleries\Http\Requests\FormRequest;
+use TypiCMS\Modules\Galleries\Models\Gallery;
 use TypiCMS\Modules\Galleries\Repositories\GalleryInterface;
 
 class AdminController extends BaseAdminController
@@ -16,29 +17,29 @@ class AdminController extends BaseAdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Galleries\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(FormRequest $request)
     {
-        $model = $this->repository->create($request->all());
+        $gallery = $this->repository->create($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $gallery);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  $model
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Galleries\Models\Gallery            $gallery
+     * @param \TypiCMS\Modules\Galleries\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update($model, FormRequest $request)
+    public function update(Gallery $gallery, FormRequest $request)
     {
         $this->repository->update($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $gallery);
     }
 }
