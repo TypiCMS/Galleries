fbc995384d7859413d79d2c970115836935e00ca
Samuel De Backer <sdebacker@gmail.com>
2015-12-31 11:48:20 +0100

    create and edit methods

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -15,6 +15,32 @@ class AdminController extends BaseAdminController
     }
 
     /**
+     * Create form for a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        $model = $this->repository->getModel();
+
+        return view('core::admin.create')
+            ->with(compact('model'));
+    }
+
+    /**
+     * Edit form for the specified resource.
+     *
+     * @param \TypiCMS\Modules\Galleries\Models\Gallery $gallery
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit(Gallery $gallery)
+    {
+        return view('core::admin.edit')
+            ->with(['model' => $gallery]);
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param \TypiCMS\Modules\Galleries\Http\Requests\FormRequest $request
