4f6e19b60ecac9762d6a780de3bc1172c5505646
Samuel De Backer <sdebacker@gmail.com>
2016-03-16 20:22:13 +0100

    unneeded attribute

--- src/resources/views/admin/files.blade.php
+++ src/resources/views/admin/files.blade.php
@@ -51,7 +51,7 @@
                         <span class="fa fa-fw fa-file-o" ng-switch-default></span>
                     </td>
                     <td>
-                        <input class="form-control input-sm" min="0" type="number" value="@{{ model.position }}" name="position" ng-model="model.position" ng-change="update(model)">
+                        <input class="form-control input-sm" min="0" type="number" name="position" ng-model="model.position" ng-change="update(model)">
                     </td>
                     <td>@{{ model.file }}</td>
                     <td>@{{ model.alt_attribute }}</td>
