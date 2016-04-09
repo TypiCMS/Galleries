646bfb3baf72826c34ec10a0846f7afc5d3e6afc
Samuel De Backer <sdebacker@gmail.com>
2016-04-09 13:44:36 +0200

    $module removed

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,14 +1,12 @@
 @extends('core::admin.master')
 
-@section('title', trans($module.'::global.name'))
+@section('title', trans('galleries::global.name'))
 
 @section('main')
 
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
-    <a href="{{ route('admin::create-'.str_singular($module)) }}" class="btn-add" title="@lang($module.'::global.New')">
-        <i class="fa fa-plus-circle"></i><span class="sr-only">@lang($module.'::global.New')</span>
-    </a>
+    @include('core::admin._button-create', ['module' => 'galleries'])
 
     <h1>
         <span>@{{ models.length }} @choice('galleries::global.galleries', 2)</span>
@@ -47,7 +45,7 @@
                 <tr ng-repeat="model in displayedModels">
                     <td typi-btn-delete action="delete(model)"></td>
                     <td>
-                        @include('core::admin._button-edit')
+                        @include('core::admin._button-edit', ['module' => 'galleries'])
                     </td>
                     <td typi-btn-status action="toggleStatus(model)" model="model"></td>
                     <td>
