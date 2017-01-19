bbfef7e7feb8cb7bc0a01d3620f09ace3cd1695f
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:02:01 +0100

    array alignment removed

--- src/resources/lang/en/global.php
+++ src/resources/lang/en/global.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name'                               => 'Galleries',
-    'galleries'                          => 'gallery|galleries',
-    'New'                                => 'New gallery',
-    'Edit'                               => 'Edit gallery',
-    'Back'                               => 'Back to galleries',
+    'name' => 'Galleries',
+    'galleries' => 'gallery|galleries',
+    'New' => 'New gallery',
+    'Edit' => 'Edit gallery',
+    'Back' => 'Back to galleries',
     'Save your gallery, then add files.' => 'Save your gallery, then add files.',
 ];
--- src/resources/lang/fr/global.php
+++ src/resources/lang/fr/global.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name'                               => 'Galeries',
-    'galleries'                          => 'galerie|galeries',
-    'New'                                => 'Nouvelle galerie',
-    'Edit'                               => 'Modifier la galerie',
-    'Back'                               => 'Retour à la liste des galeries',
+    'name' => 'Galeries',
+    'galleries' => 'galerie|galeries',
+    'New' => 'Nouvelle galerie',
+    'Edit' => 'Modifier la galerie',
+    'Back' => 'Retour à la liste des galeries',
     'Save your gallery, then add files.' => 'Enregistrez d’abord la galerie, puis ajoutez des fichiers.',
 ];
